5b22d9e3fa5eacff59d725a316b8b0ccf17ab215
Mathias Kriebel <mathias.kriebel@gmail.com>
2024-11-08 15:13:15 -0300

    feat: hiding overview on list view when mobile

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 
 import { IMovie } from '../api/types';
 import { getGenreLabel } from '../utils/genre';
+import { isMobile } from '../utils/user';
 
 interface MovieCardProps {
     movie: IMovie;
@@ -14,11 +15,14 @@ const MovieCard: React.FC<MovieCardProps> = ({ movie, onClick }) => {
     const handleClick = () => {
         onClick(movie.id);
     };
+
+    const overview = isMobile() ? "": (movie.overview.length > 300 ? movie.overview.substring(0,300) + '...' : movie.overview);
+
     return (
         <div className="card" onClick={handleClick}>
             <img src={'https://image.tmdb.org/t/p/w600_and_h900_bestv2/' + movie.poster_path} alt={movie.title}/>
             <h2>{movie.title}</h2>
-            <p>{movie.overview.length > 300 ? movie.overview.substring(0,300) + '...' : movie.overview}</p>
+            <p>{overview}</p>
             <p><strong>Lançamento:</strong> {movie.release_date}</p>
             <p><strong>Gênero:</strong> {movie.genre_ids.map(id => getGenreLabel(id)).join(', ')}</p>
             <p><strong>Nota:</strong> {movie.vote_average.toFixed(1)}</p>
